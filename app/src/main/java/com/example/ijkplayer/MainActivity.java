db5ac20a754b074d9cb446e55fc25c2d5be63f95
linyuqiang <linyuqiang@100tal.com>
2020-10-15 00:36:49 +0800

    Mod:播放地址，有assert崩溃

--- app/src/main/java/com/example/ijkplayer/MainActivity.java
+++ app/src/main/java/com/example/ijkplayer/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.ijkplayer;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
 public class MainActivity extends Activity {
+    IjkMediaPlayer ijkMediaPlayer;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,7 +26,37 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
+        SurfaceView surfaceView = findViewById(R.id.surface);
+        ijkMediaPlayer = new IjkMediaPlayer();
+        try {
+            ijkMediaPlayer.setDataSource("http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8");
+            ijkMediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        surfaceView.getHolder().setFormat(PixelFormat.RGBA_8888); // 使用RGBA_8888渲染视频(必须调用)
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                ijkMediaPlayer.setDisplay(surfaceHolder);
+                ijkMediaPlayer.start();
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                ijkMediaPlayer.pause();
+            }
+        });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ijkMediaPlayer.release();
+    }
 }
