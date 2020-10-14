ef37b70c545b339c45829e3276bbd74712ec2002
linyuqiang <linyuqiang@100tal.com>
2020-10-15 00:05:51 +0800

    Mod:第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ijkplayer_cmake
 使用cmake编译ijkplayer
+
+下载ijkplayer
+https://github.com/bilibili/ijkplayer.git
+放在app/src/main
+在 app\src\main\ijkplayer\ijkmedia\ijkplayer 下面创建 ijkversion.h
+添加
+#ifndef IJK_VERSION_H
+#define IJK_VERSION_H
+#define IJKPLAYER_VERSION "1";
+#endif
+
+下载soundtouch
+git clone --depth 10 -b ijk-r0.1.2-dev https://github.com/Bilibili/soundtouch.git ijksoundtouch
+放在app/src/main/ijkplayer/ijkmedia
+
+下载ijkplayer
+git clone --depth 10 -b ijk-r0.2.1-dev https://github.com/Bilibili/libyuv.git
+放在app/src/main/ijkplayer/ijkmedia
---
+++ app/src/main/java/com/example/ijkplayer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ijkplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("ijkplayer");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+    }
+
+}
